2da13fe4975af9bd34d014224680a9afb88df53d
Devjack404 <juansabarofek@gmail.com>
2026-02-26 14:22:56 +0700

    feat: folder basic_dsa

---
+++ Basic_DSA/index.js
@@ -0,0 +1,10 @@
+let nilai = 80
+let lulus = "Selamat kamu lulusi !!"
+let gagal = "Maaf kamu belum lulus kali ini.... jangan menyerah !!"
+
+if (nilai >= 75){
+    console.log(lulus)
+}
+else {
+    console.log(gagal)
+}
